82384ce403d3cc4d071662fae70e8c501703828f
Amaan Ahmad <20dpee018ct@manuu.edu.in>
2025-07-11 15:45:32 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
+          base: '/CertificateGenerator/',
         }
       }
     };
